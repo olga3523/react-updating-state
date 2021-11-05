3b69848ffbff5bd983147f5933c28eee2df9b034
OLGA <olichka23.85@gmail.com>
2021-11-04 20:18:50 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,45 @@
-// Code ClickityClick Component Here
+import React from "react";
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+        hasBeenClicked: false,
+        theme: 'blue',
+        addressInfo: {
+            street: null,
+            number: null,
+            city: null,
+            country: null
+        },  
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+        hasBeenClicked: true
+    }, () => console.log(this.state.hasBeenClicked))
+      
+    console.log(this.state.hasBeenClicked)
+      
+    this.setState({
+        addressInfo: {
+          city: "New York City",
+        },
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.addressInfo.city ? this.state.addressInfo.city : "not"}</p>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+  }
+}
+
+export default ClickityClick;
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ButtonCounter from './components/ButtonCounter';
+import ClickityClick from './components/ClickityClick';
+import LightSwitch from './components/LightSwitch';
 
 ReactDOM.render(
   <div>
-    Mount Components Here
+    <ClickityClick />
+    <ButtonCounter />
+    <LightSwitch />
   </div>,
   document.getElementById('root')
 );
